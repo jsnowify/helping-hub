83d7f210aca26bb580c6cf00a54eee670eeaaec8
Snowi <snowicambronero@gmail.com>
2024-05-11 08:43:45 +0800

    fixed the welcome ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Welcome from "./components/Welcome";
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center content-center place-content-center">
+      <div className="flex flex-col justify-center h-dvh gap-4">
         <Welcome />
-        <Link to="/login" className="justify-center ">
-          <button>Explore</button>
+        <Link to="/login" className="flex justify-center ">
+          <button className="bg-black w-40 h-10 rounded-full text-white hover:shadow-2xl">
+            Explore
+          </button>
         </Link>
       </div>
     </>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,8 @@
 const Welcome = () => {
   return (
     <>
-      <div className="flex justify-center item-center flex-col text-center">
-        <h1 className="text-3xl font-bold">Helping Hub</h1>
+      <div className="flex justify-center item-center flex-col text-center gap-4">
+        <h1 className="text-7xl font-bold">Helping Hub</h1>
         <p>Build your own commuity</p>
       </div>
     </>
