d70b93f470bbac91a0209958295f481a659bc6ca
Snowi <snowicambronero@gmail.com>
2024-05-11 09:35:40 +0800

    improve the ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,13 @@ import LoginButton from "./LoginButton";
 export const Login = () => {
   return (
     <>
-      <h1>Welcome Back</h1>
-      <LoginButton />
+      <div className="flex h-dvh flex-col justify-center gap-10">
+        <h1 className="flex justify-center font-bold text-6xl">Welcome Back</h1>
+        <LoginButton className="bg-black w-40 h-10 rounded-full text-white hover:shadow-2xl" />
+        <footer className="flex justify-center">
+          <span className="font-bold mr-1">Status: </span>Under Development
+        </footer>
+      </div>
     </>
   );
 };
--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -3,6 +3,12 @@ export const PageNotFound = () => {
     <div className="flex justify-center flex-col items-center gap-3 h-dvh bg-black text-white">
       <h1 className="font-bold text-5xl ">404: Page Not Found</h1>
       <p>The page you requested could not be found.</p>
+      <footer className="mt-20">
+        <span className="font-bold">Developed by:</span> Snowi
+      </footer>
+      <footer>
+        <span className="font-bold">Status: </span>Under Development
+      </footer>
     </div>
   );
 };
